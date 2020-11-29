eaeb1e0b24ef90c1b896217ecb00e7cff1af6241
mimi <mireille.trempe@gmail.com>
2020-11-29 10:52:24 -0500

    chore(cleanup): won't be using postgres

---
+++ services/Feats.Evaluations/Features/IsOnController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Feats.CQRS.Queries;
+using Feats.Domain.Validations;
+using Feats.Evaluations.Features.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Feats.Evaluations.Features
+{
+    [ApiController]
+    [Route("features")]
+    public class IsOnController
+    {
+        private readonly IHandleQuery<IsFeatureOnQuery, bool> _handler;
+
+        public IsOnController(IHandleQuery<IsFeatureOnQuery, bool> handdler)
+        {
+            this._handler = handdler;
+        }
+
+        [Route("{path:string}/{name:string}")]
+        public async Task<bool> Get(string path, string name)
+        {
+            path.Required(nameof(path));
+            name.Required(nameof(name));
+
+            return await this._handler.Handle(new IsFeatureOnQuery
+            {
+                Path = path,
+                Name = name,
+            });
+        }
+    }
+}
