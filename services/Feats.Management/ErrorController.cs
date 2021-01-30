248474dbebbeb4fd55855a53dbde695f9d1249b9
mimi <mireille.trempe@gmail.com>
2021-01-30 09:02:53 -0500

    feat(errors): report errors wit proper http status
    https://github.com/dotnet-feats/feats/projects/1#card-52518941

---
+++ services/Feats.Management/ErrorController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using Feats.Domain.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Feats.Management
+{
+    [ExcludeFromCodeCoverage]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            var errorContext = this.HttpContext.Features.Get<IExceptionHandlerFeature>();
+            
+            switch (errorContext.Error)
+            {
+                case ValidationException ex:
+                    return this.Problem(
+                        statusCode: (int)HttpStatusCode.BadRequest,
+                        title: "Validation Exception",
+                        detail: ex.Message
+                    );
+                case NotFoundException ex:
+                    return this.Problem(
+                        statusCode: (int)HttpStatusCode.NotFound,
+                        title: "Not Found Exception",
+                        detail: ex.Message
+                    );
+                case ConflictException ex:
+                    return this.Problem(
+                        statusCode: (int)HttpStatusCode.Conflict,
+                        title: "Not Found Exception",
+                        detail: ex.Message
+                    );
+                default:
+                    return this.Problem(
+                        statusCode: (int)HttpStatusCode.InternalServerError,
+                        title: "Unhandled Exception",
+                        detail: errorContext.Error.Message
+                    );
+            }
+        }
+    }
+}
